d1847a2a0bb4de37d11d0eca4c5e74eabc0c05f9
Axil Vigo <axil-pacha@hotmail.com>
2019-05-01 21:23:18 -0400

    Adding python Project

---
+++ 01-Python Object and Data Structures Basics/Data Types/Numbers.py
@@ -0,0 +1,17 @@
+
+
+#Going through basic operators in Python3
+
+print(1+2)  #Addition
+
+print(2-1)  #Substraction
+
+print(2*1)  #Multiplication
+
+print(2/1)  #Division: PERFORMS TRUE DIVISION BY DEFAULT (WILL RETURN A DECIMAL NUMBER EVERYTIME)
+
+print(2%1)  #Modulus
+
+#A special feature of Python3 is that you can compute the power of a number using the '**' operator
+
+print(2**2) #Will display 4
