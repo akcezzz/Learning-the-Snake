943e6c2ad840b982366837891f2c559d59db312e
Axil Vigo <axil-pacha@hotmail.com>
2019-05-01 22:17:11 -0400

    Modifying numbers and variable assignment

---
+++ 01-Python Object and Data Structures Basics/Data Types/VariableAssignments.py
@@ -0,0 +1,12 @@
+#In Python3, it is possible to reassign an EXISTING VARIABLE to a DIFFERENT variable type
+
+#The type() function lets you verify the type of a variable
+
+a = 5 #There is no need to specify the type in Python
+print(a, ' is of type ', type(a))
+
+a = 5.5
+print(a, ' is of type ' ,type(a))
+
+
+
