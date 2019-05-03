9287cf3a47866c4343706f62ab431ec114824abf
Axil Vigo <axil-pacha@hotmail.com>
2019-05-03 15:31:50 -0400

    Adding comparison operators section

---
+++ 02-Python Comparison Operators /Comparison Operators.py
@@ -0,0 +1,27 @@
+###         Comparison Operators            ###
+
+#Are the same as in any other programming language
+
+# ==: equality
+# !-: Not equal
+# > : Greater than
+# < : Smaller than
+# >=: Greater or equal
+# <=: Smaller or equal
+
+#Logical operators
+
+#Are different than in C/C++
+
+#and operator
+#or operator
+#not operator
+
+print(2>3 and 1<2) #Will return a false, (same as &&)
+
+print('A' == 'a' or 1==1) #Will return a true, (same as ||)
+
+
+print(not 1==1) #Will return a false (returns always return the opposite of a comparison result (same as !)
+
+print(1<2==1) #Will compute 1<2 first, then 2 == 1 (returns a false)
