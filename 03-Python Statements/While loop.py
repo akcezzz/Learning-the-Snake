ead1d8b5dcb59a53020d308ff95d5487b958d81b
Axil Vigo <axil-pacha@hotmail.com>
2019-05-03 17:44:05 -0400

    While loop section completed

---
+++ 03-Python Statements/While loop.py
@@ -0,0 +1,18 @@
+###         While loops         ###
+
+#Almost same as in C/C++
+
+x = 0
+
+while x<5:
+    print(f'The value of x is {x}')
+    x++
+else:       #It is possible to attach an else to x
+    print('X IS NOT LESS THAN 5')
+
+#continue, break and pass statements
+
+#continue: same as in c++
+#break: same as in c++
+#pass: does nothing at all
+
